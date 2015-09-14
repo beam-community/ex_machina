7f657f90c2f0fc7d6feaab4de9ef38ddd70cf5f8
Paul Smith <catalystmediastudios@gmail.com>
2015-09-14 14:36:45 -0400

    Extract functions and add Elixir documentation

--- lib/anvil.ex
+++ lib/anvil.ex
@@ -1,5 +1,42 @@
 defmodule Anvil do
+  @moduledoc """
+  Defines functions for generating data
+
+  ## Examples
+
+      defmodule MyApp.Anvil do
+        use Anvil
+
+        def factory(:config) do
+          # Factories can be plain maps
+          %{url: "http://example.com"}
+        end
+
+        def factory(:article) do
+          %Article{
+            title: "My Awesome Article"
+          }
+        end
+
+        def factory(:comment, opts) do
+          %Comment{
+            body: "This is great!",
+            article_id: assoc(opts, :article).id
+          }
+        end
+
+        def create_record(map) do
+          # This example uses Ecto to save records
+          MyApp.Repo.insert!(map)
+        end
+      end
+  """
+
   defmodule UndefinedFactory do
+    @moduledoc """
+    Error raised when trying to build or create a factory that is undefined.
+    """
+
     defexception [:message]
 
     def exception(factory_name) do
@@ -13,32 +50,110 @@ defmodule Anvil do
       @before_compile unquote(__MODULE__)
 
       def assoc(opts, factory_name) do
-        if Map.has_key?(opts, factory_name) do
-          Map.get(opts, factory_name)
-        else
-          create(factory_name)
-        end
+        Anvil.assoc(__MODULE__, opts, factory_name)
       end
 
       def build(factory_name, opts \\ %{}) do
-        opts = Enum.into(opts, %{})
-        factory(factory_name, opts) |> Map.merge(opts)
+        Anvil.build(__MODULE__, factory_name, opts)
       end
 
       def create(factory_name, opts \\ %{}) do
-        build(factory_name, opts) |> create_record
+        Anvil.create(__MODULE__, factory_name, opts)
       end
 
       def create_pair(factory_name, opts \\ %{}) do
-        create_list(2, factory_name, opts)
+        Anvil.create_pair(__MODULE__, factory_name, opts)
       end
 
       def create_list(number_of_factorys, factory_name, opts \\ %{}) do
-        Enum.map(1..number_of_factorys, fn(_) ->
-          create(factory_name, opts)
-        end)
+        Anvil.create_list(__MODULE__, number_of_factorys, factory_name, opts)
       end
     end
+  end
+
+  @doc """
+  Gets a factory from the passed in opts, or creates if none is present
+
+  ## Examples
+
+      opts = %{user: %{name: "Someone"}}
+      # Returns opts.user
+      assoc(opts, :user) 
+
+      opts = %{}
+      # Creates and returns new instance based on :user factory
+      assoc(opts, :user)
+  """
+  def assoc(module, opts, factory_name) do
+    if Map.has_key?(opts, factory_name) do
+      Map.get(opts, factory_name)
+    else
+      Anvil.create(module, factory_name)
+    end
+  end
+
+  @doc """
+  Builds a factory with the passed in factory_name
+
+  ## Example
+
+      def factory(:user) do
+        %{name: "John Doe", admin: false}
+      end
+
+      # Returns %{name: "John Doe", admin: true}
+      build(:user, admin: true)
+  """
+  def build(module, factory_name, opts \\ %{}) do
+    opts = Enum.into(opts, %{})
+    module.factory(factory_name, opts) |> Map.merge(opts)
+  end
+
+  @doc """
+  Builds a factory with the passed in factory_name and saves with create_record
+
+  ## Example
+
+      def create_record(record) do
+        # This example uses Ecto
+        MyApp.Repo.insert!(record)
+      end
+
+      def factory(:user) do
+        %{name: "John Doe", admin: false}
+      end
+
+      # Saves and returns %{name: "John Doe", admin: true}
+      create(:user, admin: true)
+  """
+  def create(module, factory_name, opts \\ %{}) do
+    Anvil.build(module, factory_name, opts) |> module.create_record
+  end
+
+  @doc """
+  Creates and returns 2 records with the passed in factory_name and opts
+
+  ## Example
+
+      # Returns a list of 2 users
+      create_pair(:user)
+  """
+  def create_pair(module, factory_name, opts \\ %{}) do
+    Anvil.create_list(module, 2, factory_name, opts)
+  end
+
+  @doc """
+  Creates and returns X records with the passed in factory_name and opts
+
+  ## Example
+
+      # Returns a list of 3 users
+      create_pair(3, :user)
+  """
+  def create_list(module, number_of_factories, factory_name, opts \\ %{}) do
+    Enum.map(1..number_of_factories, fn(_) ->
+      Anvil.create(module, factory_name, opts)
+    end)
   end
 
   defmacro __before_compile__(_env) do
--- mix.exs
+++ mix.exs
@@ -5,8 +5,11 @@ defmodule Anvil.Mixfile do
     [app: :anvil,
      version: "0.0.1",
      elixir: "~> 1.0",
+     source_url: "https://github.com/thoughtbot/anvil",
+     homepage_url: "https://github.com/thoughtbot/anvil",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: [extras: ["README.md"]],
      deps: deps]
   end
 
@@ -27,6 +30,9 @@ defmodule Anvil.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.9", only: :dev},
+      {:earmark, ">= 0.0.0", only: :dev}
+    ]
   end
 end
