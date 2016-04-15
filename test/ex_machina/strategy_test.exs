c77cd5a292afd268d583e5e547c2a0db76e4b477
Paul Smith <paulcsmith@users.noreply.github.com>
2016-04-15 08:45:28 -0400

    Add name_from_struct for strategies (#116)
    Options passed to handling functions are now a map which makes it easier
    to pattern match. It also includes the calling factory module in the
    options.
    
    name_from_struct/1 is added so that people can create strategies that
    define callbacks or need to call other functions with the factory name.
    
    Closes #111. See discussion for more details on why this strategy was
    chosen.

--- test/ex_machina/strategy_test.exs
+++ test/ex_machina/strategy_test.exs
@@ -20,8 +20,14 @@ defmodule ExMachina.StrategyTest do
     end
   end
 
+  test "name_from_struct returns the factory name based on passed in struct" do
+    assert ExMachina.Strategy.name_from_struct(%{__struct__: User}) == :user
+    assert ExMachina.Strategy.name_from_struct(%{__struct__: TwoWord}) == :two_word
+    assert ExMachina.Strategy.name_from_struct(%{__struct__: NameSpace.TwoWord}) == :two_word
+  end
+
   test "defines functions based on the strategy name" do
-    strategy_options = [foo: :bar]
+    strategy_options = %{foo: :bar, factory_module: Factory}
 
     Factory.build(:user) |> Factory.json_encode
     built_user = Factory.build(:user)
