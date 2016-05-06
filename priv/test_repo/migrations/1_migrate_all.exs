86776156faf65458951d3aeab8ebd88fdbaf54f3
Josh Steiner <josh@jsteiner.me>
2016-05-06 10:19:30 -0400

    Move all migrations to a single file
    We use an in memory database that is reset for each test run. Since we
    don't care about incremental changes, it's a lot easier to make all
    changes in a single file to see the entire state of the database at one
    time.

--- priv/test_repo/migrations/1_migrate_all.exs
+++ priv/test_repo/migrations/1_migrate_all.exs
@@ -1,7 +1,12 @@
-defmodule ExMachina.TestRepo.Migrations.CreateArticles do
+defmodule ExMachina.TestRepo.Migrations.MigrateAll do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      add :name, :string
+      add :admin, :boolean
+    end
+
     create table(:articles) do
       add :title, :string
       add :author_id, :integer
--- priv/test_repo/migrations/20151030144844_add_settings_to_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule ExMachina.TestRepo.Migrations.AddSettingsToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :settings, :map
-    end
-  end
-end
