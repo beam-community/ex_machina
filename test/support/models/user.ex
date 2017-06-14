2984966c4291385821111207bfb5f35384f99a83
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2017-06-14 03:04:39 +0200

    Fix user has_many posts association

--- test/support/models/user.ex
+++ test/support/models/user.ex
@@ -7,7 +7,7 @@ defmodule ExMachina.User do
     field :net_worth, :decimal
     field :password, :string, virtual: true
 
-    has_many :articles, ExMachina.Article
+    has_many :articles, ExMachina.Article, foreign_key: :author_id
     has_many :editors, through: [:articles, :editor]
     has_one :best_article, ExMachina.Article
   end
