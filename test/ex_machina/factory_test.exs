09c2fa792a9b6d518297f626fae1b54d89909eb6
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2017-07-05 00:35:03 -0300

    Add a test for composed factory with imported machines

--- test/ex_machina/factory_test.exs
+++ test/ex_machina/factory_test.exs
@@ -28,6 +28,13 @@ defmodule ExMachina.EnterpriseTest do
     use ExMachina
     use Japan
     use China
+
+    machine :compound do
+      %{
+        paper: build(:paper),
+        iron: build(:iron)
+      }
+    end
   end
 
   test "simple factories should be able to build" do
@@ -36,7 +43,11 @@ defmodule ExMachina.EnterpriseTest do
   end
 
   test "`use Factory` should define __using__ with factories" do
-    assert Company.build(:paper) == %{paper: true, type: "Something"}
-    assert Company.build(:iron) == %{iron: true}
+    paper = %{paper: true, type: "Something"}
+    iron = %{iron: true}
+
+    assert Company.build(:paper) == paper
+    assert Company.build(:iron) == iron
+    assert Company.build(:compound) == %{paper: paper, iron: iron}
   end
 end
