acae9253415dda4914facce968f1e9a1dc0fe286
German Velasco <germsvel@users.noreply.github.com>
2017-09-01 11:30:11 -0400

    Support structs in string_params_for/2 (#245)
    `string_params_for/2` currently fails when a struct is passed for a
    field. The error comes when a function from the enumerable protocol is
    called on the struct (assuming it's a plain map). So the following
    example would fail,
    
    ```elixir
    Factory.string_params_for(:model, some_field: %SomeStruct{key: value})
    ```
    
    The behavior of `string_params_for/2` with a struct should be to convert
    it into a plain map with strings as keys. So in the example above, we
    would expect the result from `string_params_for/2` to include
    `%{"some_field" => %{"key" => value}}`
    
    This commit does this by using `Map.from_struct/1` to turn the struct
    into plain map before continuing with the process of converting the
    atoms into strings.

--- lib/ex_machina/ecto.ex
+++ lib/ex_machina/ecto.ex
@@ -316,6 +316,9 @@ defmodule ExMachina.Ecto do
   defp convert_atom_keys_to_strings(values) when is_list(values) do
     Enum.map(values, &convert_atom_keys_to_strings/1)
   end
+  defp convert_atom_keys_to_strings(%{__struct__: _} = record) when is_map(record) do
+    Map.from_struct(record) |> convert_atom_keys_to_strings()
+  end
   defp convert_atom_keys_to_strings(record) when is_map(record) do
     Enum.reduce record, Map.new, fn({key, value}, acc) ->
       Map.put(acc, to_string(key), convert_atom_keys_to_strings(value))
