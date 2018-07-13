24ae3c8738d0add4278f45998b0a3b2c0121bd82
German Velasco <germsvel@gmail.com>
2018-07-13 16:01:35 -0400

    Upgrade to CircleCI 2
    From CircleCi,
    
    ```
    Projects currently running on CircleCI 1.0 will no longer be supported
    after August 31, 2018. Please migrate to CircleCI 2.0.
    ```
    
    So we add CircleCI 2 config file that is as close as possible to our
    current setup.

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,7 @@ config :ex_machina, ExMachina.TestRepo,
   hostname: "localhost",
   database: "ex_machina_test",
   adapter: Ecto.Adapters.Postgres,
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  username: "postgres"
 
 config :logger, level: :warn
