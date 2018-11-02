4714e7b96e1458262c8f043c0376c1f71f70a4fe
German Velasco <germsvel@gmail.com>
2018-11-02 16:20:03 -0400

    Clean up deps in mix.exs
    There were a couple of things that were not exactly accurate in `deps`:
    
    - `ecto_sql` should be optional and only version 3.0 or greater.
    - remove `poison` as dependency since we now have `jason`
    - set `jason` dep as `only: :test` since we do not want to require it
    for those who use ex_machina (thought they'll likely use it if they use
    ecto), but we do need it for our tests.

--- mix.exs
+++ mix.exs
@@ -37,10 +37,9 @@ defmodule ExMachina.Mixfile do
       {:ex_doc, "~> 0.14", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ecto, "~> 2.2 or ~> 3.0", optional: true},
-      {:jason, "~> 1.0", optional: true},
-      {:ecto_sql, "~> 2.2 or ~> 3.0", only: :test},
-      {:postgrex, "~> 0.14.0", only: :test},
-      {:poison, "~> 3.0", only: :test}
+      {:ecto_sql, "~> 3.0", optional: true},
+      {:jason, "~> 1.0", only: :test},
+      {:postgrex, "~> 0.14.0", only: :test}
     ]
   end
 
