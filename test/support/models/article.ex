a3182ce07296f082ef600584414355c48013f1de
German Velasco <germsvel@gmail.com>
2018-12-21 12:00:23 -0500

    Format elixir code
    We use `mix format` to format all our elixir code.
    
    Since we want to check that the code is formatted, we update the docker
    image in circle CI to elixir 1.7.4.
    
    And since we are now setting a version in CI, we also add a
    `.tool-versions` with elixir 1.7.4 and erlang 21 so that contributors
    can use consistent versions.

--- test/support/models/article.ex
+++ test/support/models/article.ex
@@ -2,12 +2,12 @@ defmodule ExMachina.Article do
   use Ecto.Schema
 
   schema "articles" do
-    field :title, :string
-    field :visits, :decimal
+    field(:title, :string)
+    field(:visits, :decimal)
 
-    belongs_to :author, ExMachina.User
-    belongs_to :editor, ExMachina.User
-    belongs_to :publisher, ExMachina.Publisher
-    has_many :comments, ExMachina.Comment
+    belongs_to(:author, ExMachina.User)
+    belongs_to(:editor, ExMachina.User)
+    belongs_to(:publisher, ExMachina.Publisher)
+    has_many(:comments, ExMachina.Comment)
   end
 end
--- test/support/models/user.ex
+++ test/support/models/user.ex
@@ -2,13 +2,13 @@ defmodule ExMachina.User do
   use Ecto.Schema
 
   schema "users" do
-    field :name, :string
-    field :admin, :boolean
-    field :net_worth, :decimal
-    field :password, :string, virtual: true
+    field(:name, :string)
+    field(:admin, :boolean)
+    field(:net_worth, :decimal)
+    field(:password, :string, virtual: true)
 
-    has_many :articles, ExMachina.Article, foreign_key: :author_id
-    has_many :editors, through: [:articles, :editor]
-    has_one :best_article, ExMachina.Article, foreign_key: :author_id
+    has_many(:articles, ExMachina.Article, foreign_key: :author_id)
+    has_many(:editors, through: [:articles, :editor])
+    has_one(:best_article, ExMachina.Article, foreign_key: :author_id)
   end
 end
