2f1dffda5ca44479b426888fc8b2463dd652232f
German Velasco <germsvel@gmail.com>
2020-11-10 16:12:32 -0500

    Organize tests within describe blocks
    Organize tests in `ex_machina_test.exs` and `ex_machina/ecto_test.exs`
    within describe blocks so they're easier to reason about.

--- test/ex_machina_test.exs
+++ test/ex_machina_test.exs
@@ -49,108 +49,116 @@ defmodule ExMachinaTest do
     end
   end
 
-  test "sequence/2 sequences a value" do
-    assert "me-0@foo.com" == Factory.build(:email).email
-    assert "me-1@foo.com" == Factory.build(:email).email
-  end
+  describe "sequence" do
+    test "sequence/2 sequences a value" do
+      assert "me-0@foo.com" == Factory.build(:email).email
+      assert "me-1@foo.com" == Factory.build(:email).email
+    end
 
-  test "sequence/1 shortcut for creating sequences" do
-    assert "Post Title0" == Factory.build(:article).title
-    assert "Post Title1" == Factory.build(:article).title
-  end
-
-  test "raises a helpful error if the factory is not defined" do
-    assert_raise ExMachina.UndefinedFactoryError, fn ->
-      Factory.build(:foo)
+    test "sequence/1 shortcut for creating sequences" do
+      assert "Post Title0" == Factory.build(:article).title
+      assert "Post Title1" == Factory.build(:article).title
     end
   end
 
-  test "build/2 returns the matching factory" do
-    assert Factory.build(:user) == %{
-             id: 3,
-             name: "John Doe",
-             admin: false
-           }
-  end
+  describe "build/2" do
+    test "raises a helpful error if the factory is not defined" do
+      assert_raise ExMachina.UndefinedFactoryError, fn ->
+        Factory.build(:foo)
+      end
+    end
 
-  test "build/2 merges passed in options as keyword list" do
-    assert Factory.build(:user, admin: true) == %{
-             id: 3,
-             name: "John Doe",
-             admin: true
-           }
-  end
+    test "build/2 returns the matching factory" do
+      assert Factory.build(:user) == %{
+               id: 3,
+               name: "John Doe",
+               admin: false
+             }
+    end
 
-  test "build/2 merges passed in options as a map" do
-    assert Factory.build(:user, %{admin: true}) == %{
-             id: 3,
-             name: "John Doe",
-             admin: true
-           }
-  end
+    test "build/2 merges passed in options as keyword list" do
+      assert Factory.build(:user, admin: true) == %{
+               id: 3,
+               name: "John Doe",
+               admin: true
+             }
+    end
 
-  test "build/2 raises if passing invalid keys to a struct factory" do
-    assert_raise KeyError, fn ->
-      Factory.build(:struct, doesnt_exist: true)
+    test "build/2 merges passed in options as a map" do
+      assert Factory.build(:user, %{admin: true}) == %{
+               id: 3,
+               name: "John Doe",
+               admin: true
+             }
+    end
+
+    test "build/2 raises if passing invalid keys to a struct factory" do
+      assert_raise KeyError, fn ->
+        Factory.build(:struct, doesnt_exist: true)
+      end
+    end
+
+    test "build/2 allows factories to have full control of provided arguments" do
+      assert Factory.build(:comment, name: "James") == %{
+               author: "James Doe",
+               username: "James-0",
+               name: "James"
+             }
+    end
+
+    test "build/2 allows custom (non-map) factories to be built" do
+      assert Factory.build(:room_number, floor: 5) == "500"
+      assert Factory.build(:room_number, floor: 5) == "501"
     end
   end
 
-  test "build/2 allows factories to have full control of provided arguments" do
-    assert Factory.build(:comment, name: "James") == %{
-             author: "James Doe",
-             username: "James-0",
-             name: "James"
-           }
+  describe "build_pair/2" do
+    test "build_pair/2 builds 2 factories" do
+      records = Factory.build_pair(:user, admin: true)
+
+      expected_record = %{
+        id: 3,
+        name: "John Doe",
+        admin: true
+      }
+
+      assert records == [expected_record, expected_record]
+    end
   end
 
-  test "build/2 allows custom (non-map) factories to be built" do
-    assert Factory.build(:room_number, floor: 5) == "500"
-    assert Factory.build(:room_number, floor: 5) == "501"
-  end
+  describe "build_list/3" do
+    test "build_list/3 builds the factory the passed in number of times" do
+      records = Factory.build_list(3, :user, admin: true)
 
-  test "build_pair/2 builds 2 factories" do
-    records = Factory.build_pair(:user, admin: true)
+      expected_record = %{
+        id: 3,
+        name: "John Doe",
+        admin: true
+      }
 
-    expected_record = %{
-      id: 3,
-      name: "John Doe",
-      admin: true
-    }
-
-    assert records == [expected_record, expected_record]
-  end
-
-  test "build_list/3 builds the factory the passed in number of times" do
-    records = Factory.build_list(3, :user, admin: true)
-
-    expected_record = %{
-      id: 3,
-      name: "John Doe",
-      admin: true
-    }
-
-    assert records == [expected_record, expected_record, expected_record]
-  end
-
-  test "build_list/3 handles the number 0" do
-    assert [] = Factory.build_list(0, :user)
-  end
-
-  test "raises helpful error when using old create functions" do
-    assert_raise RuntimeError, ~r/create\/1 has been removed/, fn ->
-      Factory.create(:user)
+      assert records == [expected_record, expected_record, expected_record]
     end
 
-    assert_raise RuntimeError, ~r/create\/2 has been removed/, fn ->
-      Factory.create(:user, admin: true)
+    test "build_list/3 handles the number 0" do
+      assert [] = Factory.build_list(0, :user)
     end
 
-    assert_raise RuntimeError, ~r/create_pair\/2 has been removed/, fn ->
-      Factory.create_pair(:user, admin: true)
-    end
+    test "raises helpful error when using old create functions" do
+      assert_raise RuntimeError, ~r/create\/1 has been removed/, fn ->
+        Factory.create(:user)
+      end
 
-    assert_raise RuntimeError, ~r/create_list\/3 has been removed/, fn ->
-      Factory.create_list(3, :user, admin: true)
+      assert_raise RuntimeError, ~r/create\/2 has been removed/, fn ->
+        Factory.create(:user, admin: true)
+      end
+
+      assert_raise RuntimeError, ~r/create_pair\/2 has been removed/, fn ->
+        Factory.create_pair(:user, admin: true)
+      end
+
+      assert_raise RuntimeError, ~r/create_list\/3 has been removed/, fn ->
+        Factory.create_list(3, :user, admin: true)
+      end
     end
   end
 end
