4fc5729de15e397861603ce012f69f6c0f7133c7
German Velasco <germsvel@gmail.com>
2020-12-12 05:06:33 -0500

    Use a real struct (not named struct) in ExMachinaTest
    We used to have a struct factory defined as `%{__struct__: FooBar}` but
    it was confusing since the factory name was `struct_factory`.
    
    So we change to be `foo_bar_factory` and create a module with a struct
    called `FooBar`. This has the benefit of allowing us to assert
    `%FooBar{}` in tests -- something we couldn't do with the previous
    format.

--- test/ex_machina_test.exs
+++ test/ex_machina_test.exs
@@ -1,6 +1,10 @@
 defmodule ExMachinaTest do
   use ExUnit.Case
 
+  defmodule FooBar do
+    defstruct [:name]
+  end
+
   defmodule Factory do
     use ExMachina
 
@@ -38,10 +42,8 @@ defmodule ExMachinaTest do
       }
     end
 
-    def struct_factory do
-      %{
-        __struct__: Foo.Bar
-      }
+    def foo_bar_factory do
+      %FooBar{}
     end
 
     def comment_factory(attrs) do
@@ -108,7 +110,7 @@ defmodule ExMachinaTest do
 
     test "build/2 raises if passing invalid keys to a struct factory" do
       assert_raise KeyError, fn ->
-        Factory.build(:struct, doesnt_exist: true)
+        Factory.build(:foo_bar, doesnt_exist: true)
       end
     end
 
